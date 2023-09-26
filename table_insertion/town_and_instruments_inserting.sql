0002436bcffe306b593a8a90719fccd85ef7323b
Danielpozd <52414159+Danielpozd@users.noreply.github.com>
2023-09-26 18:16:52 +0300

    Немного наполнил базу

---
+++ table_insertion/town_and_instruments_inserting.sql
@@ -0,0 +1,33 @@
+insert into town (id, name)
+values
+	(null, "novosibirsk"),
+	(null, "moscow"),
+	(null, "saint-petersburg"),
+	(null, "omsk"),
+	(null, "krasnnoyarsk"),
+	(null, "ekaterinburg"),
+	(null, "tomsk"),
+	(null, "chelyabinsk"),
+	(null, "naberezhnie chelni"),
+	(null, "samara"),
+	(null, "kaliningrad"),
+	(null, "vladivostok"),
+	(null, "ufa"),
+	(null, "tollyati"),
+	(null, "Krasnogorsk"),
+	(null, "svedlogorsk"),
+	(null, "sochi"),
+	(null, "kazan"),
+	(null, "voronezh");
+	
+insert into instrument (id, name)
+values
+	(null, "piano"),
+	(null, "organ"),
+	(null, "violin"),
+	(null, "cello"),
+	(null, "guitar"),
+	(null, "clarinet"),
+	(null, "tuba"),
+	(null, "lozhki"),
+	(null, "drums");
